edde85f554bf39c1c68d4baa215f9a2b5ece4729
heath3rq <105904149+heath3rq@users.noreply.github.com>
2023-04-25 01:31:56 +0000

    add recommendation notebook and cleanup

--- youtube.py
+++ youtube.py
@@ -9,7 +9,7 @@ def retrieve_shows(genres: list[str], num_show: int) -> None:
     """Retrieve Youtube show information (i.e., show title, genre, and url)."""
     # Set the API credentials and parameters
     youtube = googleapiclient.discovery.build(
-        "youtube", "v3", developerKey=os.environ['YOUTUBE_API'])
+        "youtube", "v3", developerKey=os.environ['YOUTUBE_API_2'])
     search_params = {
         "type": "video",
         "part": "snippet",
@@ -37,7 +37,7 @@ def retrieve_shows(genres: list[str], num_show: int) -> None:
                 # Get the video ID and retrieve the video information
                 video_id = search_result["id"]["videoId"]
                 video_response = youtube.videos().list(
-                    part="snippet",
+                    part="snippet, statistics",
                     id=video_id,
                     hl="en_US"
                 ).execute()
@@ -58,4 +58,4 @@ def retrieve_shows(genres: list[str], num_show: int) -> None:
 
 
 if __name__ == "__main__":
-    retrieve_shows(["Comedy", "Drama", "Action", "Horror"], 200)
+    retrieve_shows(["Comedy", "Drama", "Action", "Horror", "Music", "Art"], 500)
